48e521912f163cb3e04a0f905481c9722aad7228
nelotgrom <nelotgrom@gmail.com>
2019-03-24 19:00:51 +0300

    file: complited task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var j = 1;
+  var i = 2;
+  var a = [];
+  do { 
+    if (base % i === 0) {
+      a[j] = i;
+      j++;
+      base = base / i;
+    } else {
+      i++;
+    }
+  }
+  while (i < base);
+
+  a[j] = i;
+  var res = [];
+  a.forEach(function(e){
+      res[e] = 1 + ~~res[e];
+  })
+
+  var nok = [];
+  for(var k in res){
+    nok.push([+k, res[k]]);  
+  }
+  
+  var nullContainer = [];
+  for (var w = 0; w < nok.length; w++ ) {
+    var summ = 0;
+    var nk = 1;
+    for (var l = 1; nk >= 1; l++) { 
+      nk = Math.floor(number / Math.pow(nok[w][0], l));
+      summ += nk;
+    }
+    nullContainer.push(Math.floor(summ / nok[w][1])); 
+  } 
+
+  return nullContainer.sort(function(o, p){
+    return o - p;
+  })[0];
 }
+
